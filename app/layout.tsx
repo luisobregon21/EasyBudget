40eb6edb744993cdc14253514eab2084cf8f2641
luisObregon-wicho <lo@gulpdata.com>
2026-05-08 23:31:28 -0400

    feat: install sonner, add Toaster to root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { PwaRegister } from "@/components/pwa-register";
+import { Toaster } from "sonner";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -32,6 +33,7 @@ export default function RootLayout({
       <body className="min-h-full flex flex-col">
         <PwaRegister />
         {children}
+        <Toaster position="bottom-center" richColors />
       </body>
     </html>
   );
